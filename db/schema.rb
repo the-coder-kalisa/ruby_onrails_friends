2c93b71a4d37a8ba8fe1d8d218af1c3b92c00b54
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-03 15:29:02 +0200

    added firends well

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_092837) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_094912) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
